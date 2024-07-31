9efc6de44a300a1a502d13a46a62677260ac1bba
jaypreak <jayesh.preak2003@gmail.com>
2024-07-31 20:25:34 +0530

    chud gaye guru

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,32 @@
-import "@/styles/globals.css"
+
 import { Inter as FontSans } from "next/font/google"
 
 import { cn } from "@/lib/utils"
+import './globals.css'
+import { Metadata } from "next"
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
 
+export const metaData: Metadata = {
+  title: "MonkeyWrite",
+  description: "Ape write together",
+  // image: "https://example.com/image.jpg",
+  // url: "https://example.com",
+}
+
 export default function RootLayout({ children }: {children: React.ReactNode}) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "min-h-screen font-sans antialiased",
           fontSans.variable
         )}
       >
-        ...
+        {children}
       </body>
     </html>
   )
