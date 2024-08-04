5394c389a290133afb01da9a53a1072670ccbfad
jaypreak <jayesh.preak2003@gmail.com>
2024-08-04 19:49:56 +0530

    change icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,8 +6,8 @@ const Header = ({ children }: HeaderProps) => {
     return (
         <div className='header'>
             <Link href='/' className='md:flex-1'>
-                <Image src='/assets/icons/logo.svg' alt='logo' width={120} height={32} className='hidden md:block' />
-                <Image src='/assets/icons/logo-icon.svg' alt='logo' width={32} height={32} className='mr-2 md:hidden' />
+                <Image src='/assets/icons/monkey-write.svg' alt='logo' width={120} height={32} className='hidden md:block' />
+                <Image src='/assets/icons/jb.svg' alt='logo' width={32} height={32} className='mr-2 md:hidden' />
             </Link>{children}
         </div>
     )
