6f969a4a480794891045f87600f50a7c33d35bdf
jaypreak <jayesh.preak2003@gmail.com>
2024-08-12 19:20:07 +0530

    head

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,30 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-const Header = ({ children }: HeaderProps) => {
-    return (
-        <div className='header'>
-            <Link href='/' className='md:flex-1'>
-                <Image src='/assets/icons/monkey-write.svg' alt='logo' width={120} height={32} className='hidden md:block' />
-                <Image src='/assets/icons/jb.svg' alt='logo' width={32} height={32} className='mr-2 md:hidden' />
-            </Link>{children}
-        </div>
-    )
-}
+const Header = ({ children, className }: HeaderProps) => {
+  return (
+    <div className={cn("header", className)}>
+      <Link href="/" className="md:flex-1">
+        <Image
+          src="/assets/icons/monkey-write.svg"
+          alt="logo"
+          width={120}
+          height={32}
+          className="hidden md:block"
+        />
+        <Image
+          src="/assets/icons/jb.svg"
+          alt="logo"
+          width={32}
+          height={32}
+          className="mr-2 md:hidden"
+        />
+      </Link>
+      {children}
+    </div>
+  );
+};
 
-export default Header
+export default Header;
