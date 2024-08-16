0d9a3d0583ae0597b640d821d57575593deb77df
jaypreak <jayesh.preak2003@gmail.com>
2024-08-16 21:44:03 +0530

    uh yes

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -1,25 +1,39 @@
 "use client";
 
 import { RoomProvider, ClientSideSuspense } from "@liveblocks/react/suspense";
-import React from "react";
+import React, { useRef, useState } from "react";
 import Loader from "./Loader";
 import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 import { Editor } from "./editor/Editor";
 import Header from "./Header";
 import ActiveCollaborators from "./ActiveCollaborators";
+import { Input } from "./ui/input";
 
 const CollaborativeRoom = ({
   roomId,
   roomMetadata,
 }: CollaborativeRoomProps) => {
+  const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
   const [editing, setEditing] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
     <RoomProvider id={roomId}>
       <ClientSideSuspense fallback={<Loader />}>
         <div className="collaborative-room">
           <Header>
-            <div className="flex w-fit items-center justify-center gap-2">
-              <p className="document-title">Abhijeet sex</p>
+            <div
+              ref={containerRef}
+              className="flex w-fit items-center justify-center gap-2"
+            >
+              {editing && !loading ? (
+                <Input />
+              ) : (
+                <p className="document-title">{documentTitle}</p>
+              )}
+              {/* <p className="document-title">Abhijeet sex</p> */}
             </div>
             <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
               <ActiveCollaborators />
@@ -39,6 +53,3 @@ const CollaborativeRoom = ({
 };
 
 export default CollaborativeRoom;
-function useState(arg0: boolean): [any, any] {
-  throw new Error("Function not implemented.");
-}
