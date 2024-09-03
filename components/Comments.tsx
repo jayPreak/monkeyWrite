3a8ebb4484d06cde8d257a304fe57c27433f181c
jaypreak <jayesh.preak2003@gmail.com>
2024-09-03 20:16:15 +0530

    comments

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import { useIsThreadActive } from "@liveblocks/react-lexical";
 import { Composer, Thread } from "@liveblocks/react-ui";
 import { useThreads } from "@liveblocks/react/suspense";
