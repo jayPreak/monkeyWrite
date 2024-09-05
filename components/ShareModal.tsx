a29451f468330393a605842781f77fff99474b61
jaypreak <jayesh.preak2003@gmail.com>
2024-09-05 19:40:16 +0530

    ewe

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -1,6 +1,22 @@
-import React from "react";
+"use client";
+import { useSelf } from "@liveblocks/react/suspense";
+import React, { useState } from "react";
 
-const ShareModal = () => {
+const ShareModal = ({
+  roomId,
+  collaborators,
+  creatorId,
+  currentUserType,
+}: ShareDocumentDialogProps) => {
+  const user = useSelf();
+
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [userType, setUserType] = useState<UserType>("viewer");
+
+  const share;
   return <div>ShareModal</div>;
 };
 
