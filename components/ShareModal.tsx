00b78f9b284d2608d19d055286d031f324f2ff4e
jaypreak <jayesh.preak2003@gmail.com>
2024-09-05 19:42:01 +0530

    dialowog

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -1,6 +1,14 @@
 "use client";
 import { useSelf } from "@liveblocks/react/suspense";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+} from "@radix-ui/react-dialog";
 import React, { useState } from "react";
+import { DialogHeader } from "./ui/dialog";
 
 const ShareModal = ({
   roomId,
@@ -16,8 +24,21 @@ const ShareModal = ({
   const [email, setEmail] = useState("");
   const [userType, setUserType] = useState<UserType>("viewer");
 
-  const share;
-  return <div>ShareModal</div>;
+  const shareDocumentHandler = async () => {};
+  return (
+    <Dialog>
+      <DialogTrigger>Open</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default ShareModal;
