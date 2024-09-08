918c84842519c1fb435948f93df2d41c165c21d7
jaypreak <jayesh.preak2003@gmail.com>
2024-09-08 21:12:55 +0530

    stuff

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -1,16 +1,23 @@
 "use client";
-import { useSelf } from "@liveblocks/react/suspense";
+
 import {
   Dialog,
-  DialogTrigger,
   DialogContent,
-  DialogTitle,
   DialogDescription,
-} from "@radix-ui/react-dialog";
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { useSelf } from "@liveblocks/react/suspense";
 import React, { useState } from "react";
-import { DialogHeader } from "./ui/dialog";
 import { Button } from "./ui/button";
 import Image from "next/image";
+// import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+// import UserTypeSelector from "./UserTypeSelector";
+// import Collaborator from "./Collaborator";
+// import { updateDocumentAccess } from "@/lib/actions/room.actions";
 
 const ShareModal = ({
   roomId,
@@ -26,7 +33,19 @@ const ShareModal = ({
   const [email, setEmail] = useState("");
   const [userType, setUserType] = useState<UserType>("viewer");
 
-  const shareDocumentHandler = async () => {};
+  // const shareDocumentHandler = async () => {
+  //   setLoading(true);
+
+  //   await updateDocumentAccess({
+  //     roomId,
+  //     email,
+  //     userType: userType as UserType,
+  //     updatedBy: user.info,
+  //   });
+
+  //   setLoading(false);
+  // }
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger>
@@ -36,22 +55,60 @@ const ShareModal = ({
         >
           <Image
             src="/assets/icons/share.svg"
+            alt="share"
             width={20}
             height={20}
-            alt="Share"
             className="min-w-4 md:size-5"
           />
           <p className="mr-1 hidden sm:block">Share</p>
         </Button>
       </DialogTrigger>
-      <DialogContent>
+      <DialogContent className="shad-dialog">
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogTitle>Manage who can view this project</DialogTitle>
           <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            Select which users can view and edit this document
           </DialogDescription>
         </DialogHeader>
+
+        <Label htmlFor="email" className="mt-6 text-blue-100">
+          Email address
+        </Label>
+        <div className="flex items-center gap-3">
+          <div className="flex flex-1 rounded-md bg-dark-400">
+            <Input
+              id="email"
+              placeholder="Enter email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="share-input"
+            />
+            <UserTypeSelector userType={userType} setUserType={setUserType} />
+          </div>
+          <Button
+            type="submit"
+            onClick={shareDocumentHandler}
+            className="gradient-blue flex h-full gap-1 px-5"
+            disabled={loading}
+          >
+            {loading ? "Sending..." : "Invite"}
+          </Button>
+        </div>
+
+        <div className="my-2 space-y-2">
+          <ul className="flex flex-col">
+            {collaborators.map((collaborator) => (
+              <Collaborator
+                key={collaborator.id}
+                roomId={roomId}
+                creatorId={creatorId}
+                email={collaborator.email}
+                collaborator={collaborator}
+                user={user.info}
+              />
+            ))}
+          </ul>
+        </div>
       </DialogContent>
     </Dialog>
   );
